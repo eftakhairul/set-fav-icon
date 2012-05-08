dfecbb2f04482c46c342a6867b4e569403824449
Md. Eftakhairul Islam <eftakhairul@gmail.com>
2012-05-08 17:24:02 +0600

    css modification

--- save-icon-url.php
+++ save-icon-url.php
@@ -5,6 +5,7 @@ global $wpdb;
 $favIconTable = $wpdb->prefix . "fav_icon_link";
 settings_fields('set-fav-icon');
 
+//Update submit value
 if(!empty($_POST['link'])) {
     $wpdb->update($favIconTable, array('link' => $_POST['link']), array('id' => 1));
 }
@@ -15,13 +16,6 @@ $link    = (!empty($_POST['link'])) ? $_POST['link'] : $result[0]->link;
 ?>
 
 <div class="wrap">
-<!--    --><?php ////echo __FILE__?><!--<br/>-->
-<!--    --><?php //if (count($error) > 0) {
-//    foreach ($error as $e) {
-//        echo '<strong>' . $e . '</strong><br>';
-//    }
-//}
-//    ?>
     <h2>Set Favicon URL</h2>
 
     <form method="post" action="<?php echo $_SERVER['REQUEST_URI']?>">
