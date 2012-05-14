38a7ca81b1f1b86c71c3b2d228ac219a27eca12b
Md. Eftakhairul Islam <eftakhairul@gmail.com>
2012-05-14 16:44:02 +0600

    final released

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 set-fav-icon
 =================================================================================
 Authors
-
 Md. Eftakhairul Islam <eftakhairul@gmail.com>
 Sirajus Salayhin <salayhin@gmail.com>
-
 
 Versions: 1.0
 
--- save-icon-url.php
+++ save-icon-url.php
@@ -18,9 +18,16 @@ $link    = (!empty($_POST['link'])) ? $_POST['link'] : $result[0]->link;
 <div class="wrap">
     <h2>Set Favicon URL</h2>
 
+    <?php if(!empty($_POST['link'])) : ?>
+    <div class="updated inline below-h2">
+        <p>  Fav Icon Link is updated successfully.  </p>
+    </div>
+    <br/>
+    <?php endif; ?>
+    
+
     <form method="post" action="<?php echo $_SERVER['REQUEST_URI']?>">
         <?php wp_nonce_field('update-options'); ?>
-
         <label>
             Fav Icon Url:
         </label>
@@ -28,7 +35,5 @@ $link    = (!empty($_POST['link'])) ? $_POST['link'] : $result[0]->link;
         <input type="submit" class="button-primary" name="save-fav-icon"
                            value="<?php _e('Save Changes') ?>"/>
         <input type="hidden" name="action" value="update"/>
-
-
     </form>
 </div>
